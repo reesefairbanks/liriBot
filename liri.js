47cbf750d01b9a191c7df9eb4a4d35d99a168bc5
Reese Fairbanks <reese@reeses-mbp.attlocal.net>
2019-02-04 01:03:13 -0500

    Updated the Spotify function

--- liri.js
+++ liri.js
@@ -11,32 +11,37 @@
 
 //var movieData = ""
 
-var axios = require("axios");
+const axios = require("axios");
 const command = process.argv[2];
 const value = process.argv[3];
-//const spotify = require('node-spotify-api');
+const Spotify = require('node-spotify-api');
 
 if (command === "movie-this") {
     getMyMovie(value);
-} else if (command ==="concert-this"){
-getMyBands(value);
+} else if (command ==="concert-this") {
+    getMyBands(value);
+} else if (command === "spotify-this-song") {
+    getMySpotify(value);
 }
 
-//const spotify = new spotify({
-    //id:
-    //secret:
-//});
+//Spotify function 
+const spotify = new Spotify({
+  id: "39684ab0cd8c40b8ab9bc29093da0138",
+  secret: "8b5acaf4176447e4ba7ecc3457bedb29"
+});
+ 
+function getMySpotify(item) {
+    spotify.search({ type: 'track', query: item, limit: 10 }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+      console.log(data); 
+      });
+}
 
-// spotify.search({ type: 'track', query: 'All the Small Things', limits: 10 }, function(err, data) {
-//     if (err) {
-//         return console.log('Error occured: ' + err)
-//     }
-// })
 
-//const questions = [
-    //{type: 'input', name: 'name', message: 'What is your name?'}
-//];
-
+//Get bands function
 function getMyBands(artist) {
     const url = "https://rest.bandsintown.com/artists/" + artist + "/events?";
     
@@ -54,7 +59,7 @@ function getMyBands(artist) {
 }
 
 
-
+//OMDB get movie function
 function getMyMovie(movieName) {
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
